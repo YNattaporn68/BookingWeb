f1771d9f8b9ddda11cd9a04222ac1e25b6007ada
nutnaree <nutnaree.suktad@m.swu.ac.th>
2024-04-26 13:08:42 +0100

    34: fix login test

--- test/login.test.js
+++ test/login.test.js
@@ -12,6 +12,18 @@ describe('GET /LoginPage', () => {
     expect(res.text).toContain('Login');
   });
 });
-//Test Suites: 1 passed, 1 total
-//Tests:       1 passed, 1 total
+
+describe('Authentication', () => {
+
+  it('should return an error with missing credentials', async () => {
+      const res = await request(app)
+          .post('/auth/LoginPage')
+          .send({});
+      expect(res.statusCode).toEqual(200);
+      expect(res.text).toContain('Please Provide an email and password');
+  });
+});
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+
 
